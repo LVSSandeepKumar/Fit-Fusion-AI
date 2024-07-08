c6dd4a8f91b74de26cab3c919f2ecc20515046fa
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-08 10:51:00 +0530

    Backend Setup with drizzle orm and Neon Postgres

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://Fit_Fusion_AI_owner:TsE4SBRtpA1H@ep-damp-silence-a5owr5vh.us-east-2.aws.neon.tech/Fit_Fusion_AI?sslmode=require',
+    }
+  };
+  
